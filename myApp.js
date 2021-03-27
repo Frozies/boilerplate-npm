dc7dc8794cd978fc2242e28aa4707e1845b304ec
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-27 14:45:23 -0400

    Added nodemon for easy development. created an indexpage send file.

--- myApp.js
+++ myApp.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
+var indexPage = __dirname + "/views/index.html";
 
 app.get("/",function (req,res){
- res.send('Hello Express');
+  res.sendFile(indexPage);
 });
 
  module.exports = app;
