50bd8f0e5e4e080d1d079c46d209628e019b586e
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-27 15:06:38 -0400

    reverted changes

--- myApp.js
+++ myApp.js
@@ -2,10 +2,39 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
-var indexPage = __dirname + "/views/index.html";
 
-app.get("/",function (req,res){
-  res.sendFile(indexPage);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  module.exports = app;
